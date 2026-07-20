3a5dc672cae5cd0a6e8e9c737e68c331e189b4bb
Pixel Studio Plugins <affairegamer@gmail.com>
2026-07-20 23:10:45 +0200

    Preparer l'integration des icones fournies

---
+++ app/src/embedded_icons.cpp
@@ -0,0 +1,4 @@
+#include "embedded_icons.hpp"
+
+// Les icones sont dessinees dans l'interface a partir des references fournies :
+// Miiverse (vert), Nintendo eShop (orange), collections (dossier).
